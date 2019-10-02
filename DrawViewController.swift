296b25c5eac3d2e53d1caebfbb8b13c176888251
Sirarpi Bayramyan <Sirarpi1997@gmail.com>
2019-10-03 01:13:36 +0400

    Added panGestureRecognizer on CanvasView for moving Shapelayers

--- DrawViewController.swift
+++ DrawViewController.swift
@@ -10,58 +10,48 @@ import UIKit
 
 class DrawViewController: UIViewController {
     
-    var snapX:CGFloat = 40.0
+    var layerIndex = Int()
+    var layer: TypeShapeLayer?
+    var panGesture = UIPanGestureRecognizer()
+    var shapeLayerColor: UIColor = .black
+    var oldpozitions = [CGPoint]()
     
-    /// must be >= 1.0
-    var snapY:CGFloat = 1.0
     
-    /// how far to move before dragging
-    var threshold:CGFloat = 0.0
-    
-    /// the guy we're dragging
-    var selectedView:UIView?
-    
-    /// drag in the Y direction?
-    var shouldDragY = true
-    
-    /// drag in the X direction?
-    var shouldDragX = true
-    
-    let canvas = Canvas()
-    
-    @IBOutlet weak var CanvasView: Canvas!
+    @IBOutlet weak var canvasViewOutlet: Canvas!
     
     @IBAction func cleanButton(_ sender: UIButton) {
-        CanvasView.clear()
+        canvasViewOutlet.clear()
 
     }
     @IBAction func redoButton(_ sender: UIButton) {
-        CanvasView.redo()
+        canvasViewOutlet.redo()
     }
     @IBAction func undoButton(_ sender: UIButton) {
-        CanvasView.undo()
+        if canvasViewOutlet.mode == .draw {
+        canvasViewOutlet.undo()
+        }
 
     }
     
     @IBAction func slider(_ sender: UISlider) {
         sender.minimumValue = 3
         sender.maximumValue = 10
-        CanvasView.setStrokeWidth(width: CGFloat(sender.value))
+        canvasViewOutlet.setStrokeWidth(width: CGFloat(sender.value))
        // print(sender.value)
     }
     
     @IBAction func pancil(_ sender: UIButton) {
-        CanvasView.setStrokeColor(color:.black)
-        CanvasView.setStrokeWidth(width: 1)
+        canvasViewOutlet.setStrokeColor(color:.black)
+        canvasViewOutlet.setStrokeWidth(width: 1)
     }
     
     @IBAction func rubberButton(_ sender: UIButton) {
-        CanvasView.setStrokeColor(color:CanvasView.backgroundColor!)
-        CanvasView.setStrokeWidth(width: 10)
+        canvasViewOutlet.setStrokeColor(color:canvasViewOutlet.backgroundColor!)
+        canvasViewOutlet.setStrokeWidth(width: 10)
     }
   
     @IBAction func colorButton(_ sender: UIButton) {
-        CanvasView.setStrokeColor(color:sender.backgroundColor!)
+        canvasViewOutlet.setStrokeColor(color:sender.backgroundColor!)
 }
 
         
@@ -71,18 +61,74 @@ class DrawViewController: UIViewController {
     }
 
     
-
-    @IBAction func drawDrag(_ sender: UISegmentedControl) {
-        CanvasView.mode = sender.selectedSegmentIndex == 0 ? .draw : .move
-//        if (CanvasView.mode == .drag) {
-//            setupGestures()
-//        }
-    }
-}
-extension UIView {
-    func removeLayer() {
-            for item in self.layer.sublayers ?? []  {
-                    item.removeFromSuperlayer()
-            }
+    
+   
+    @IBAction func changeMode(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            canvasViewOutlet.mode = .draw
+            canvasViewOutlet.removeGestureRecognizer(panGesture)
+        default:
+            canvasViewOutlet.mode = .move
+            panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGesture(recognizer:)))
+            canvasViewOutlet.addGestureRecognizer(panGesture)
         }
+    }
+    @objc func panGesture(recognizer: UIPanGestureRecognizer) {
+            
+            switch recognizer.state {
+            case .began:
+                 canvasViewOutlet.path = UIBezierPath()
+                print( canvasViewOutlet.shapeLayers.count)
+                for (i, shapeLayer) in  canvasViewOutlet.shapeLayers.enumerated() {
+                    let loc =   canvasViewOutlet.layer.convert(recognizer.location(in:  canvasViewOutlet), to: shapeLayer.drawLayer)
+    //                CGPoint pointInLayer = [[layer presentationLayer] convertPoint:movingPoint fromLayer:self.view.layer];
+    //                CGRect layerBounds = [[layer presentationLayer] bounds];
+    //                let loc: CGPoint = shapeLayer.drawLayer.convert(recognizer.location(in: drawingViewOutlet), to: drawingViewOutlet.layer)
+    //                if (CGRectContainsPoint(layerBounds, pointInLayer)) {
+    //                    // Intersect!
+    //                }
+                    if (shapeLayer.drawLayer.path?.contains(loc))! {
+                        print("begin")
+                        layer = TypeShapeLayer(shapeLayer: shapeLayer.drawLayer, color: shapeLayerColor)
+                        layerIndex = i
+                       canvasViewOutlet.shapeLayers[layerIndex] = layer!
+                        //
+    //                    drawingViewOutlet.shapeLayers[i].movePosition = shapeLayer.drawLayer.position
+    //                    drawingViewOutlet.undoLayers.append(drawingViewOutlet.shapeLayers[i])
+                    }
+                }
+            case .changed:
+                print("changed")
+              // drawingViewOutlet.path = UIBezierPath()
+                guard let layer = layer else { return }
+                let translation = recognizer.translation(in: recognizer.view)
+                
+                CATransaction.begin();
+                CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+    //            redLayer.convert(position, to: yellowLayer
+               
+                //var layerCoordinate = layer.convert(layer.drawLayer.lo, to: drawingViewOutlet.layer)
+                layer.drawLayer.position = CGPoint(x: layer.drawLayer.position.x + translation.x, y: layer.drawLayer.position.y + translation.y)
+                
+                canvasViewOutlet.draw(canvasViewOutlet.bounds)
+                recognizer.setTranslation(CGPoint.zero, in: canvasViewOutlet)
+
+                CATransaction.commit();
+                
+                
+            case .ended:
+                print("gredu")
+                
+                guard let _ = layer else { return }
+                
+                canvasViewOutlet.shapeLayers.insert(layer!, at: layerIndex)
+                print(canvasViewOutlet.shapeLayers.count)
+            default:
+                break
+        }
+        
+    }
+    
 }
+
